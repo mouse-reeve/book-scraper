64e3c4ae0385bd6dc7b85dd29052860983e64cbc
Mouse Reeve <mouse.reeve@gmail.com>
2014-10-21 15:15:37 -0700

    Crawl based on catalog page, rather than csv
    This way their isbn -> work api doesn't shut me out after five requests.
    Sorry, librarything!

--- book_scraper/spiders/libraryThingScraper.py
+++ book_scraper/spiders/libraryThingScraper.py
@@ -1,3 +1,4 @@
+import re
 import csv
 import scrapy
 from scrapy.contrib.spiders import CrawlSpider, Rule
@@ -9,36 +10,42 @@ class LibraryThingSpider(scrapy.Spider):
     allowed_domains = ['librarything.com']
     start_urls = [];
 
-    # I should generalize this
-    fileName = 'library.csv'
+    start_urls.append('https://www.librarything.com/catalog_bottom.php?view=tripofmice')
 
-    with open(fileName, 'rb') as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            if 'isbn' in row:
-                isbn = row['isbn']
-                start_urls.append('http://www.librarything.com/isbn/' + isbn)
-            else:
-                continue
-
+    # this does not seem to be working AT ALL
     rules = [
+        #Rule(LinkExtractor(allow=['offset=\d+']), 'parse'),
+        Rule(LinkExtractor(allow=['/book/\d+']), 'parse'),
         Rule(LinkExtractor(allow=['/commonknowledge/\d+']), 'parse')
     ]
 
     def parse(self, response):
+        for link in response.xpath('//a/@href'):
+            path = link.extract()
+            if re.match('\/work\/\d+\/book\/\d+', path):
+                yield scrapy.http.Request('https://www.librarything.com/' + path)
+
         item = BookItem()
-        item['isbn'] = self.isbn
+
+        try:
+            item['isbn'] = response.xpath('//meta[@property="books:isbn"]/@content').extract()[0]
+        except:
+            pass
 
         table = response.xpath('//div[@id="fwikiContainerTablediv"]//tr')
         for row in table:
             rowData = row.extract()
             if 'Original publication date' in rowData:
-                item['year'] = row.xpath('.//a/text()').extract()[0]
+                print row.xpath('.//a/text()').extract()
+                try:
+                    item['year'] = row.xpath('.//a/text()').extract()[0]
+                except:
+                    # maybe not ideal error handling here?
+                    pass
             elif 'Important places' in rowData:
                 item['places'] = row.xpath('.//a/text()').extract()
             elif 'People/Characters' in rowData:
                 item['characters'] = row.xpath('.//a/text()').extract()
 
         yield item
-
 
